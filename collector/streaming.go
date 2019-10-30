b48b6a4023e557571a3dd6aaedf2b5761d6c3150
Dani Sim <dsim@quantopian.com>
2019-10-30 14:15:14 -0400

    Remove extra loop and unnecessary durable queue mapping.

--- collector/streaming.go
+++ collector/streaming.go
@@ -281,9 +281,6 @@ func (nc *channelsCollector) Collect(ch chan<- prometheus.Metric) {
 			ch <- prometheus.MustNewConstMetric(nc.chanLastSeq, prometheus.GaugeValue,
 				float64(channel.LastSeq), server.ID, channel.Name)
 
-			durableSubLastSent := make(map[string]float64)
-			durableSubPendingCount := make(map[string]float64)
-
 			for _, sub := range channel.Subscriptions {
 
 				// If this is a durable queue group subscription then split the
@@ -293,8 +290,6 @@ func (nc *channelsCollector) Collect(ch chan<- prometheus.Metric) {
 				if sub.IsDurable && queueName != "" {
 					subStrings := strings.Split(queueName, ":")
 					durableName, queueName = subStrings[0], subStrings[1]
-					durableSubLastSent[durableName] = float64(sub.LastSent)
-					durableSubPendingCount[durableName] = float64(sub.PendingCount)
 				}
 				labelValues := []string{server.ID, channel.Name, sub.ClientID, sub.Inbox,
 					queueName, strconv.FormatBool(sub.IsDurable), strconv.FormatBool(sub.IsOffline), durableName}
@@ -305,20 +300,8 @@ func (nc *channelsCollector) Collect(ch chan<- prometheus.Metric) {
 					float64(sub.PendingCount), labelValues...)
 				ch <- prometheus.MustNewConstMetric(nc.subsMaxInFlight, prometheus.GaugeValue,
 					float64(sub.MaxInflight), labelValues...)
-			}
-
-			for _, sub := range channel.Subscriptions {
-				durableName := sub.DurableName
-				queueName := sub.QueueName
-				if sub.IsDurable && queueName != "" {
-					subStrings := strings.Split(queueName, ":")
-					durableName, queueName = subStrings[0], subStrings[1]
-					labelValues := []string{server.ID, channel.Name, sub.ClientID, sub.Inbox,
-						queueName, strconv.FormatBool(sub.IsDurable), strconv.FormatBool(sub.IsOffline), durableName}
-					ch <- prometheus.MustNewConstMetric(nc.durableUnackdMsgs, prometheus.GaugeValue,
-						float64(channel.Msgs)-(durableSubLastSent[durableName]-
-							durableSubPendingCount[durableName]), labelValues...)
-				}
+				ch <- prometheus.MustNewConstMetric(nc.durableUnackdMsgs, prometheus.GaugeValue,
+					float64(channel.Msgs)-(float64(sub.LastSent)-float64(sub.PendingCount)), labelValues...)
 			}
 		}
 	}
